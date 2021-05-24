cafdc35252351aec71c72e73c9dab3476dfcfe40
juanDev-47 <juanp.arenas@udea.edu.co>
2021-05-23 21:55:53 -0500

    API consulta de imagenes

---
+++ README.md
@@ -0,0 +1,1 @@
+# API consulta de imagenes via PixelBay
---
+++ js/app.js
@@ -0,0 +1,140 @@
+const resultado = document.querySelector('#resultado');
+const formulario = document.querySelector('#formulario');
+const paginacionDiv = document.querySelector('#paginacion');
+
+const registroPorPagina = 30;
+let totalPaginas;
+let iterador;
+let paginaActual = 1;
+
+
+window.onload = () => {
+    formulario.addEventListener('submit', validarFormulario);
+    
+
+}
+
+function calcularPaginas(total) {
+    return parseInt(Math.ceil(total / registroPorPagina));
+}
+
+function validarFormulario(e) {
+    e.preventDefault();
+
+    const terminoBusqueda = document.querySelector('#termino').value;
+
+    if( terminoBusqueda === '') {
+        
+        mostrarAlerta('Agregar un termino de busqueda');
+        return;
+    }
+
+    buscarImagenes();
+}
+
+
+// funcion que crea la alerta de validacion del formulario
+function mostrarAlerta(mensaje) {
+
+    const existe = document.querySelector('.bg-red-100');
+
+    if(!existe){
+
+
+        const alerta = document.createElement('p');
+        alerta.classList.add('bg-red-100', 'border-red-400', 'text-red-700', 'px-4', 'py-3', 'rounded', 'max-w-lg', 'mx-auto', 'mt-6', 'text-center');
+
+        alerta.innerHTML = `
+        <strong class="font-bold">Error! </strong>
+        <span class="block sm:inline">${mensaje}</span>
+        `;
+
+        formulario.appendChild(alerta);
+
+        setTimeout(() => {
+            alerta.remove();
+        }, 2000);
+    }
+
+
+}
+
+function buscarImagenes() {
+    const termino = document.querySelector('#termino').value;
+    const key = '21758243-b230a3f8e660015b25f40db56';
+    const url = `https://pixabay.com/api/?key=${key}&q=${termino}&per_page=${registroPorPagina}&page=${paginaActual}`;
+
+    fetch(url)
+        .then(respuesta => respuesta.json())
+        .then(resultado => {
+            totalPaginas = calcularPaginas(resultado.totalHits);
+
+            mostrarImagenes(resultado.hits);
+        })
+}
+
+// Generador que va a registrar la cantidad de elementos de acuerdo a las paginas
+function *crearPaginador(total) {
+    for (let i = 0; i <= total; i++ ) {
+        yield i;
+    }
+
+}
+
+function mostrarImagenes(imagenes) {
+    while(resultado.firstChild) {
+        resultado.removeChild(resultado.firstChild);
+    }
+
+    // iterar sobre el arreglo de imagenes
+    imagenes.forEach(imagen => {
+        const { previewURL, likes, views, largeImageURL } = imagen;
+
+        resultado.innerHTML += `
+            <div class="w-1/2 md:w-1/3 lg:w-1/4 p-3 mb-4">
+                <div class="bg-white">
+                    <img class="w-full" src="${previewURL}">
+
+                    <div class="p-4">
+                        <p class="font-bold"> ${likes} <span class="font-light"> Me gusta</span> </p>
+                        <p class="font-bold"> ${views} <span class="font-light"> Veces vista</span> </p>
+
+                        <a class="block w-full bg-blue-800 hover:bg-blue-500 text-white uppercase font-bold text-center rounded mt-5 p-1" href="${largeImageURL}" target="_blank" rel="noopener noreferrer"> Ver imagen</a>
+                </div>
+            </div>
+        `;
+    });
+
+    while(paginacionDiv.firstChild) {
+        paginacionDiv.removeChild(paginacionDiv.firstChild);
+    }
+
+    imprimirPaginador();
+}
+
+function imprimirPaginador() {
+    iterador = crearPaginador(totalPaginas);
+
+    console.log(iterador.next().done);
+
+    while(true) {
+        const { value, done } = iterador.next();
+        if(done) return;
+
+        // Caso contrario, genera un boton por cada elemnto en el generador
+        const boton = document.createElement('a');
+        boton.href = "#";
+        boton.dataset.pagina = value;
+        boton.textContent = value;
+        boton.classList.add('siguiente', 'bg-yellow-400', 'px-4', 'py-1', 'mr-2', 'font-bold', 'mb-10', 'uppercase', 'rounded');
+
+        boton.onclick = () => {
+            paginaActual = value;
+
+            buscarImagenes();
+        }
+
+        paginacionDiv.appendChild(boton);
+    }
+
+}
